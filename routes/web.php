858a9786eee5ce12bf40df5296e0e1acb0599d01
likuor <likuor0528@gmail.com>
2023-11-27 18:04:40 -0800

    add: route and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [BlogController::class, 'showList'])->name('blogs');
+
