267710eb0cab17f525dd4fe3a1bfc57a8164e3d1
likuor <likuor0528@gmail.com>
2023-11-29 12:48:02 -0800

    add: seeders through factory

---
+++ database/seeders/BlogsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Blog;
+
+class BlogsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Blog::factory()->count(10)->create();
+    }
+}
